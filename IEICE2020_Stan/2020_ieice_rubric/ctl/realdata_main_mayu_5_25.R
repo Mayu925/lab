3e4e81d47968ccf0652112ed6e0d36b41e88c3fb
Mayu925 <ponkitiponpon925@gmail.com>
2021-06-22 00:19:04 +0900

    data

--- IEICE2020_Stan/2020_ieice_rubric/ctl/realdata_main_mayu_5_25.R
+++ IEICE2020_Stan/2020_ieice_rubric/ctl/realdata_main_mayu_5_25.R
@@ -1,5 +1,5 @@
-Sys.setenv("http_proxy" = "http://proxy.uec.ac.jp:8080")
-Sys.setenv("https_proxy" = "https://proxy.uec.ac.jp:8080")
+#Sys.setenv("http_proxy" = "http://proxy.uec.ac.jp:8080")
+#Sys.setenv("https_proxy" = "https://proxy.uec.ac.jp:8080")
 library(rstan)
 library(loo)
 library(psych)
@@ -14,13 +14,88 @@ source("common/util_mayu.R")
 source("common/ctl_util.R")
 
 
-setting <- list(K = 5, n_person = 34, n_item = 4, n_rater = 34, n_time = 136)
-data <- read_data(setting, paste("data/peerassessment_20151224.csv", sep=""))
-
+setting <- list(K = 5, n_person = 34, n_item = 4, n_rater = 34, n_time = 34)
+data <- read_data(setting, paste("data/mayu_data_0.csv", sep=""))
+data1 <-read_data(setting, paste("data/mayu_data_1.csv", sep=""))
+data2 <-read_data(setting, paste("data/mayu_data_2.csv", sep=""))
+data3 <-read_data(setting, paste("data/mayu_data_3.csv", sep=""))
 data$ItemID = data$ItemID + 1
+data1$TimeID = data1$TimeID - 34
+data2$TimeID = data1$TimeID - 68
+data3$TimeID = data1$TimeID - 102
+data1<- list()
+data2<- list()
+data3<- list()
+data4<- list()
+data1$K=data$K
+data2$K=data$K
+data3$K=data$K
+data4$K=data$K
+data1$J=data$J
+data2$J=data$J
+data3$J=data$J
+data4$J=data$J
+data1$I=4
+data2$I=4
+data3$I=4
+data4$I=4
+data1$R=data$R
+data2$R=data$R
+data3$R=data$R
+data4$R=data$R
+data1$T=data$T
+data2$T=data$T
+data3$T=data$T
+data4$T=data$T
+data1$N=1156
+data2$N=1156
+data3$N=1156
+data4$N=1156
+
+j1=1
+j2=1
+j3=1
+j4=1
+
+for(i in 1:4624){
+  if(data$ItemID[i] == 1){
+    data1$ItemID[j1]=data$ItemID[i]
+    data1$ExamineeID[j1]=data$ExamineeID[i]
+    data1$RaterID[j1]=data$RaterID[i]
+    data1$TimeID[j1]=data$TimeID[i]
+    data1$X[j1]=data$X[i]
+    j1=j1+1
+  }else if(data$ItemID[i] == 2){
+    data2$ItemID[j2]=data$ItemID[i]
+    data2$ExamineeID[j2]=data$ExamineeID[i]
+    data2$RaterID[j2]=data$RaterID[i]
+    data2$TimeID[j2]=data$TimeID[i]
+    data2$X[j2]=data$X[i]
+    j2=j2+1
+  }else if(data$ItemID[i] == 3){
+    data3$ItemID[j3]=data$ItemID[i]
+    data3$ExamineeID[j3]=data$ExamineeID[i]
+    data3$RaterID[j3]=data$RaterID[i]
+    data3$TimeID[j3]=data$TimeID[i]
+    data3$X[j3]=data$X[i]
+    j3=j3+1
+  }else if(data$ItemID[i] == 4){
+    data4$ItemID[j4]=data$ItemID[i]
+    data4$ExamineeID[j4]=data$ExamineeID[i]
+    data4$RaterID[j4]=data$RaterID[i]
+    data4$TimeID[j4]=data$TimeID[i]
+    data4$X[j4]=data$X[i]
+    j4=j4+1
+  }
+}
+
 
 stan <- stan_model(file=paste(dir, "stan/", model, ".stan", sep=""))
-fit <- sampling(stan, data=data, iter=1000, warmup=500, chains=3)
+fit1 <- sampling(stan, data=data, iter=1000, warmup=500, chains=3)
+summary(fit1)$summary[,"mean"]
+fit2 <- sampling(stan, data=data2, iter=1000, warmup=500, chains=3)
+fit3 <- sampling(stan, data=data3, iter=1000, warmup=500, chains=3)
+fit4 <- sampling(stan, data=data4, iter=1000, warmup=500, chains=3)
 
 source(paste(dir, "models/", model, ".R", sep=""))
 est_param <- get_estimates(fit, setting)
--- IEICE2020_Stan/2020_ieice_rubric/stan/mayu.stan
+++ IEICE2020_Stan/2020_ieice_rubric/stan/mayu.stan
@@ -28,7 +28,7 @@ parameters {
   vector[K-2] beta_rk [R];
   //vector[K] beta_rt[R, T];
   //real<lower=0> tau_r [R-1];
-  vector[K] alpha_rt[R,T];
+  matrix[R,T] alpha_rt;
   real<lower=0> pai_1r [R];
   vector[R] pai_0r;
   vector[K] e_rt[R,T];
@@ -87,10 +87,10 @@ model{
   
   for (r in 1:R){
     alpha_rt[r,1] ~ normal(0, 1);
-    e_rt[r,1] ~ normal(0, 1);
+    
     for (t in 2:T){
-      alpha_rt[r,t] ~ normal(0, 1);
-      e_rt[r,t] ~ normal(0, 1);
+      alpha_rt[r,t] ~ normal(alpha_rt[r,t-1], 1);
+      
     }
   }
   for (p in 1:R) category_est [p,] ~ normal(0, 1);
@@ -98,16 +98,16 @@ model{
   for (n in 1:N){
     //X[n] ~ categorical_logit(alpha_r[RaterID[n]]*(theta[ExamineeID[n]]-beta_rt[RaterID[n],TimeID[n]]-beta_rk[RaterID[n]]));
     //X[n] ~ categorical_logit(alpha_r[RaterID[n]]*(theta[ExamineeID[n]]-severity[RaterID[n],TimeID[n]]-beta_rk[RaterID[n]]));
-    X[n] ~ categorical_logit(alpha_r[RaterID[n]]*(c*(theta[ExamineeID[n]])-pai_0r[RaterID[n]]-pai_1r[RaterID[n]]*alpha_rt[RaterID[n],TimeID[n]]-e_rt[RaterID[n],TimeID[n]]-category_prm[RaterID[n]]));
+    X[n] ~ categorical_logit(alpha_r[RaterID[n]]*(c*(theta[ExamineeID[n]])-pai_0r[RaterID[n]]-pai_1r[RaterID[n]]*alpha_rt[RaterID[n],TimeID[n]]-category_prm[RaterID[n]]));
   }
 }
 
-/*
+
 generated quantities {
   vector[N] log_lik;
   for (n in 1:N){
     //log_lik[n] = categorical_logit_log(X[n], alpha_r[RaterID[n]]*(theta[ExamineeID[n]]-beta_rt[RaterID[n],TimeID[n]]-beta_rk[RaterID[n]]));
-    log_lik[n] = categorical_logit_log(X[n], alpha_r[RaterID[n]]*(theta[ExamineeID[n]]-severity[RaterID[n],TimeID[n]]-beta_rk[RaterID[n]]));
+    log_lik[n] = categorical_logit_log(X[n], alpha_r[RaterID[n]]*(c*(theta[ExamineeID[n]])-pai_0r[RaterID[n]]-pai_1r[RaterID[n]]*alpha_rt[RaterID[n],TimeID[n]]-category_prm[RaterID[n]]));
   }
 }
-*/
+
