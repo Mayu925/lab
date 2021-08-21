0f39c42b67aa0f3b7912f873e94df3fbba28a5ca
Mayu925 <ponkitiponpon925@gmail.com>
2021-08-21 17:50:00 +0900

    betaがNAになる問題解決

--- Mayu_R/models/mayu_previous.R
+++ Mayu_R/models/mayu_previous.R
@@ -1,7 +1,7 @@
 get_prm_list <- function(param, i, r, t){
   return(list(pai_1r = param$pai_1r[r], pai_0r = param$pai_0r[r], 
               #alpha_rt = param$alpha_rt, 
-              category_prm = append(0, param$beta_rk)))
+              category_prm = append(0, param$category_prm)))
 }
 
 logit <- function(param, category_prm, x){
@@ -17,19 +17,19 @@ get_param_size <- function(data){
 get_estimates <- function(fit1, setting){
   theta <- summary(fit1, par="theta")$summary[,"mean"]
   #alpha_rt <- convert_alpha_rt(summary(fit1, par="alpha_rt")$summary[,"mean"] ,setting$n_time, setting$n_rater)
-  category_prm <- convert_category_estimates_previous(summary(fit1, par="beta_rk")$summary[,"mean"],setting$K)
+  category_prm <- convert_category_estimates_previous(summary(fit1, par="category_prm")$summary[,"mean"],setting$K)
   pai_0r <- summary(fit1, par="pai_0r")$summary[,"mean"]
   pai_1r <- summary(fit1, par="pai_1r")$summary[,"mean"]
   param = list(theta = theta, 
                #alpha_rt = alpha_rt, 
-               beta_rk = category_prm, pai_0r = pai_0r, pai_1r = pai_1r)
+               category_prm = category_prm, pai_0r = pai_0r, pai_1r = pai_1r)
   return(param)
 }
 
 get_Rhat_stat <- function(fit1){
   RhatData <- c( 
                  #summary(fit1, par="alpha_rt")$summary[,"Rhat"],
-                 summary(fit1, par="beta_rk")$summary[,"Rhat"],
+                 summary(fit1, par="category_prm")$summary[,"Rhat"],
                  summary(fit1, par="pai_0r")$summary[,"Rhat"],
                  summary(fit1, par="pai_1r")$summary[,"Rhat"],
                  summary(fit1, par="theta")$summary[,"Rhat"])
@@ -39,14 +39,14 @@ get_Rhat_stat <- function(fit1){
 generate_true_param <- function(setting){
   theta <- rnorm(setting$n_person, 0, 1.0)
   #alpha_rt <- generate_alpha_rt(setting$n_rater, setting$n_time)
-  beta_rk <- gen_category_param_previous(setting$K)
+  category_prm <- gen_category_param_previous(setting$K)
   pai_0r <- rnorm(setting$n_rater, 0, 1.0)
   pai_1r <- rlnorm(setting$n_rater, 0, 1.0)
   theta = theta - mean(theta)
  
   param = list(theta = theta,  
                #alpha_rt = alpha_rt,
-               beta_rk = beta_rk, pai_0r = pai_0r, pai_1r = pai_1r)
+               category_prm = category_prm, pai_0r = pai_0r, pai_1r = pai_1r)
   return(param)
 }
 
@@ -54,12 +54,12 @@ get_error <- function(true_param, est_param){
   #error_alpha_rt <- calc_error(true_param$alpha_rt, est_param$alpha_rt)
   error_pai_0r <- calc_error(true_param$pai_0r, est_param$pai_0r)
   error_pai_1r <- calc_error(true_param$pai_1r, est_param$pai_1r)
-  error_category <- list(RMSE = sqrt(apply((true_param$beta_rk - est_param$beta_rk)^2, 2, mean)), 
-                         BIAS = apply((true_param$beta_rk - est_param$beta_rk), 2, mean));
+  error_category <- list(RMSE = sqrt(apply((true_param$category_prm - est_param$category_prm)^2, 2, mean)), 
+                         BIAS = apply((true_param$category_prm - est_param$category_prm), 2, mean));
   error_theta <- calc_error(true_param$theta, est_param$theta)
   rmse <- list(theta = error_theta, 
                #alpha_rt = error_alpha_rt, 
-               pai_0r = error_pai_0r, pai_1r = error_pai_1r, beta_ck = error_category)  
+               pai_0r = error_pai_0r, pai_1r = error_pai_1r, category_prm = error_category)  
   return(rmse)
 }
 
