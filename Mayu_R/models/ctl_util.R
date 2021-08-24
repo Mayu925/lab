1e2803aa8d1911836237879b36697188ce316c5a
PaperAuthor <>
2021-08-24 19:58:13 +0900

    ctl_utilから一旦使わない関数を削除

--- Mayu_R/models/ctl_util.R
+++ Mayu_R/models/ctl_util.R
@@ -3,9 +3,6 @@ get_result_statistics_common <- function(fit, data, setting){
   Rhat <- get_Rhat_stat(fit)
   SE_stat <- get_theta_se_stat(fit)
   D <- c("WAIC", waic(extract_log_lik(fit))$estimates[3,1], 
-         "ML", get_ml(fit),
-         "AIC", aic(data, est_param),
-         "BIC", bic(data, est_param),
          "RhatMax", Rhat$maxRhat,
          "RhatMean", Rhat$meanRhat,
          "SE Mean", SE_stat$mean_se,
@@ -26,12 +23,14 @@ convert_category_estimates <- function(category_prm, N, K){
   }  
   return(mat)
 }
+
 convert_category_estimates_previous <- function(category_prm, K){
     prm = category_prm[1:K-1]
     prm = append(prm, -1*sum(prm))
     mat = t(data.frame(prm))
   return(mat)
 }
+
 convert_alpha_rt <- function(alpha_rt, T, R){
   for(r in 1:R){
     alrt = alpha_rt[((r-1)*T+1):((r-1)*T+T)]
@@ -74,7 +73,6 @@ gen_category_param_previous <- function(K){
   return(category)
 }
 
-
 read_data <- function(setting, filename){
   data <- read.table(filename, header=TRUE,sep=",")
   colnames(data) <- c("ExamineeID", "ItemID", "RaterID", "Score", "TimeID")
@@ -90,14 +88,6 @@ get_alpha_estimates_with_restriction <- function(d){
 
 get_estimates_with_mean_restriction <- function(d){
   return(append(-1*sum(d), d))
-}
-
-aic <- function(data, param){
-  return(-2*get_likelihood(data, param) + 2*(get_param_size(data)));
-}
-
-bic <- function(data, param){
-  return(-2*get_likelihood(data, param) + (get_param_size(data)) *log(data$J));
 }
 
 log_normal_generator <- function(n, mean, sd) {
@@ -118,21 +108,6 @@ calc_error <- function(a, b){
   return(list(RMSE = calc_rmse(a, b), BIAS = calc_bias(a, b)))
 }
 
-to_wide_format <- function(data){
-  X = array(NA, dim = c(data$J, data$I, data$R, data$T))
-  for (n in 1:length(data$X)){
-    X[data$ExamineeID[n], data$ItemID[n], data$RaterID[n],data$TimeID[n]] = data$X[n];
-  }
-  return(X)
-}
-
-to_long_format <- function(data, X){
-  for (n in 1:length(data$X)){
-    data$X[n] <- X[data$ExamineeID[n], data$ItemID[n], data$RaterID[n],data$TimeID[n]];
-  }
-  return(data)
-}
-
 generate_constrained_alpha <- function(N){
   const_alpha <- log_normal_generator(N, mean=1, sd=0.4)
   const_alpha[2:N] = exp(log(const_alpha[2:N]) - mean(log(const_alpha[2:N])))
@@ -149,12 +124,6 @@ generate_alpha_rt <- function(R,T){
     }
   }
   return(const_alpha_rt)
-}
-
-get_ml <- function(fit){
-  ms <- rstan::extract(fit)
-  theta <- apply(ms$log_lik, 1, sum)
-  return(-2*(1/sum(1/theta)*length(theta)))
 }
 
 generate_data <- function(param, setting){
@@ -183,54 +152,6 @@ generate_data <- function(param, setting){
   data=list(K=setting$K, J=setting$n_person, I=setting$n_item, R=setting$n_rater, T=setting$n_time, N=nrow(data), 
             ItemID=data$ItemID, ExamineeID=data$ExamineeID, RaterID=data$RaterID, TimeID=data$TimeID, X=data$Score)
   return(data)
-}
-
-get_eap <- function(X, data, param){
-  Xh <- seq(-3, 3, length=50)
-  AXh <- dnorm(Xh, 0, 1)
-  theta_eap <- c(1:data$J)
-  names(theta_eap) <- c(1:data$J)
-  for(j in 1:data$J){
-    Z1 = 0.0;
-    Z2 = 0.0;
-    for(h in 1:length(Xh)){
-      LL = 0.0;
-      for(i in 1:data$I){
-        for(r in 1:data$R){
-          for(t in 1:data$T){
-            k <- X[j, i, r, t]
-            if(is.na(k) == FALSE){
-              p <- prob(get_prm_list(param, i, r, t), k, Xh[h])              
-              LL = LL + log(p);
-            }
-          }
-        }
-      }
-      Z1 = Z1 + exp(LL)*AXh[h]*Xh[h];
-      Z2 = Z2 + exp(LL)*AXh[h];
-    }
-    theta_eap[j] = (Z1 / Z2);
-  }  
-  return(theta_eap)
-}
-
-get_likelihood <- function(data, param){
-  X <- to_wide_format(data)
-  LL <- 0
-  for(j in 1:data$J){
-    for(i in 1:data$I){
-      for(r in 1:data$R){
-        for(t in 1:data$T){
-          k <- X[j, i, r, t]
-          if(is.na(k) == FALSE){
-            p <- prob(get_prm_list(param, i, r, t), k, param$theta[j])              
-            LL = LL + log(p) 
-          }
-        }
-      }
-    }
-  }
-  return (LL)       
 }
 
 get_theta_se_stat <- function(fit){
