78bcbd6c77b67960ffb808b982813c12e07bcb04
Mayu925 <ponkitiponpon925@gmail.com>
2021-12-10 15:16:46 +0900

    1210

--- Mayu_R/ctl/realdata_main_mayu_previous.R
+++ Mayu_R/ctl/realdata_main_mayu_previous.R
@@ -10,38 +10,26 @@ source("models/ctl_util_previous.R")
 
 model = "mayu_previous"
 
-setting1 <- list(K = 5, n_person = 134, n_rater = 17, n_time = 4)
-setting2 <- list(K = 5, n_person = 34, n_item = 1, n_rater = 34, n_time = 5)
-setting3 <- list(K = 5, n_person = 34, n_item = 1, n_rater = 34, n_time = 10)
-data1 <- read_data(setting1, paste("data/mayu_data_3_t=3.csv", sep=""))
-data2 <-read_data(setting2, paste("data/mayu_data_3_t=5.csv", sep=""))
-data3 <-read_data(setting3, paste("data/mayu_data_3_t=10.csv", sep=""))
-data1 <-read_data(setting1, paste("data/data_o_2-5-11.csv", sep=""))
-data1$ItemID = data1$ItemID -2
-data2$ItemID = data2$ItemID -2
-data3$ItemID = data3$ItemID -2
+
+setting <- list(K = 5, n_person = 134, n_rater = 6, n_time = 4)
+data <-read_data(setting, paste("data/data_bias.csv", sep=""))
+
+setting <- list(K = 5, n_person = 134, n_rater = 11, n_time = 4)
+data <-read_data(setting, paste("data/data_n_bias.csv", sep=""))
+
+setting <- list(K = 5, n_person = 134, n_rater = 17, n_time = 4)
+data <- read_data(setting, paste("data/data_o_2-5-11.csv", sep=""))
+
+
 
 stan <- stan_model(file=paste("stan/", model, ".stan", sep=""))
-fit1 <- sampling(stan, data=data1, iter=1000, warmup=500, chains=3)
-fit2 <- sampling(stan, data=data2, iter=1000, warmup=500, chains=3)
-fit3 <- sampling(stan, data=data3, iter=1000, warmup=500, chains=3)
-#fit4 <- sampling(stan, data=data4, iter=1000, warmup=500, chains=3)
+fit <- sampling(stan, data=data, iter=1000, warmup=500, chains=3)
 
 source(paste( "models/", "mayu_previous", ".R", sep=""))
-est_param1 <- get_estimates(fit1, setting1)
-D1 <- get_result_statistics_common(fit1, data1, setting1)
+est_param <- get_estimates(fit, setting)
+D <- get_result_statistics_common(fit, data, setting)
 write.csv(t(matrix(D1, nrow=2)), paste("output/realdata/MCMC_mayu/previous/", model, "t_3.csv", sep=""))
 #write.csv(t(matrix(est_param1, nrow=34)), paste("output/realdata/parameters/mayu/previous/", model, "t_3.csv", sep=""))
-
-est_param2 <- get_estimates(fit2, setting2)
-D2 <- get_result_statistics_common(fit2, data2, setting2)
-write.csv(t(matrix(D2, nrow=2)), paste("output/realdata/MCMC_mayu/previous/", model, "t_5.csv", sep=""))
-#write.csv(t(matrix(est_param2$theta, nrow=34)), paste(dir, "output/realdata/parameters/mayu/previous/", model, "t_5.csv", sep=""))
-
-est_param3 <- get_estimates(fit3, setting3)
-D3 <- get_result_statistics_common(fit3, data3, setting3)
-write.csv(t(matrix(D3, nrow=2)), paste("output/realdata/MCMC_mayu/previous/", model, "t_10.csv", sep=""))
-#write.csv(t(matrix(est_param1$theta, nrow=34)), paste(dir, "output/realdata/parameters/mayu/previous", model, "t_10.csv", sep=""))
 
 wbic3 <- -mean(rowSums(extract(fit1)$log_lik))
 wbic5 <- -mean(rowSums(extract(fit2)$log_lik))
@@ -55,4 +43,16 @@ write.csv(SD, paste(dir, "output/realdata/SE/", model, "1.csv", sep=""))
 
 sink(paste(dir, "output/realdata/parameters/", model, "_param.txt", sep=""))
 sink()
-
+pai_1rt <- matrix(nrow = setting$n_rater, ncol = 4)
+for(t in 1:4){
+  for(r in 1:setting$n_rater){
+    pai_1rt[r,t] = est_param$pai_1r[r]*(t/4)
+  }
+}
+# 結果描画用
+plot(est_param$theta)
+plot(est_param$beta_rk)
+for(r in 1:setting1$n_rater){
+  plot(est_param1$pai_0r[r]+pai_1rt[r,],xlab="TimeID",ylab="pai_0r*pai_1r*t", type="l",xlim=c(1, 4), xaxp=c(1, 4, 3), ylim=c(-1,1), col=r, lwd = 1)
+  par(new=T)
+}
--- Mayu_R/ctl/realdata_main_mayu_proposed.R
+++ Mayu_R/ctl/realdata_main_mayu_proposed.R
@@ -10,66 +10,33 @@ model = "mayu_proposed"
 
 source("models/ctl_util.R")
 
-setting1 <- list(K = 5, n_person = 134, n_rater = 6, n_time = 4)
-#setting2 <- list(K = 5, n_person = 34, n_item = 1, n_rater = 34, n_time = 5)
-#setting3 <- list(K = 5, n_person = 34, n_item = 1, n_rater = 34, n_time = 10)
-data1 <- read_data(setting1, paste("data/data_o_2-5-11.csv", sep=""))
-data1 <-read_data(setting1, paste("data/data_n_bias.csv", sep=""))
-data3 <-read_data(setting1, paste("data/data_bias.csv", sep=""))
-#data4 <-read_data(setting, paste("data/mayu_data_3.csv", sep=""))
+setting <- list(K = 5, n_person = 134, n_rater = 6, n_time = 4)
+data <-read_data(setting, paste("data/data_bias.csv", sep=""))
+
+setting <- list(K = 5, n_person = 134, n_rater = 11, n_time = 4)
+data <-read_data(setting, paste("data/data_n_bias.csv", sep=""))
+
+setting <- list(K = 5, n_person = 134, n_rater = 17, n_time = 4)
+data <- read_data(setting, paste("data/data_o_2-5-11.csv", sep=""))
 
 
-data1$ItemID = data1$ItemID -2
-data2$ItemID = data2$ItemID -2
-data3$ItemID = data3$ItemID -2
-#data4$ItemID = data4$ItemID + 1
 
 stan <- stan_model(file=paste("stan/", model, ".stan", sep=""))
-fit1 <- sampling(stan, data=data1, iter=1000, warmup=500, chains=3)
-fit2 <- sampling(stan, data=data2, iter=1000, warmup=500, chains=3)
-fit3 <- sampling(stan, data=data3, iter=1000, warmup=500, chains=3)
-#fit4 <- sampling(stan, data=data4, iter=1000, warmup=500, chains=3)
+fit <- sampling(stan, data=data, iter=1000, warmup=500, chains=3)
 
 source(paste("models/", "mayu_proposed", ".R", sep=""))
-est_param1 <- get_estimates(fit1, setting1)
-D1 <- get_result_statistics_common(fit1, data1, setting1)
-write.csv(t(matrix(D1, nrow=2)), paste( "output/realdata/MCMC_mayu/proposed/", model, ".csv", sep=""))
-write.csv(t(matrix(est_param1$theta, nrow=34)), paste( "output/realdata/parameters/mayu/proposed/", model, "_theta_t_3.csv", sep=""))
-write.csv(t(matrix(est_param1$alpha_r, nrow=34)), paste( "output/realdata/parameters/mayu/proposed/", model, "_alpha_r_t_3.csv", sep=""))
-write.csv(t(matrix(est_param1$beta_rt, nrow=34)), paste( "output/realdata/parameters/mayu/proposed/", model, "_beta_rt_3.csv", sep=""))
-write.csv(t(matrix(est_param1$beta_rk, nrow=34)), paste( "output/realdata/parameters/mayu/proposed/", model, "_beta_rk_3.csv", sep=""))
+est_param <- get_estimates(fit, setting)
+D <- get_result_statistics_common(fit, data, setting)
+write.csv(t(matrix(D, nrow=2)), paste( "output/realdata/MCMC_mayu/proposed/", model, ".csv", sep=""))
+write.csv(t(matrix(est_param$theta, nrow=34)), paste( "output/realdata/parameters/mayu/proposed/", model, "_theta.csv", sep=""))
+write.csv(t(matrix(est_param$alpha_r, nrow=34)), paste( "output/realdata/parameters/mayu/proposed/", model, "_alpha_r.csv", sep=""))
+write.csv(t(matrix(est_param$beta_rt, nrow=34)), paste( "output/realdata/parameters/mayu/proposed/", model, "_beta_r.csv", sep=""))
+write.csv(t(matrix(est_param$beta_rk, nrow=34)), paste( "output/realdata/parameters/mayu/proposed/", model, "_beta_rk.csv", sep=""))
 
-est_param2 <- get_estimates(fit2, setting2)
-D2 <- get_result_statistics_common(fit2, data2, setting2)
-write.csv(t(matrix(D2, nrow=2)), paste("output/realdata/MCMC_mayu/proposed/", model, "t_5.csv", sep=""))
-write.csv(t(matrix(est_param2$theta, nrow=34)), paste( "output/realdata/parameters/mayu/proposed/", model, "_theta_t_5.csv", sep=""))
-write.csv(t(matrix(est_param2$alpha_r, nrow=34)), paste( "output/realdata/parameters/mayu/proposed/", model, "_alpha_r_t_5.csv", sep=""))
-write.csv(t(matrix(est_param2$beta_rt, nrow=34)), paste( "output/realdata/parameters/mayu/proposed/", model, "_beta_rt_5.csv", sep=""))
-write.csv(t(matrix(est_param2$beta_rk, nrow=34)), paste( "output/realdata/parameters/mayu/proposed/", model, "_beta_rk_5.csv", sep=""))
+wbic <- -mean(rowSums(extract(fit)$log_lik))
 
 
-est_param3 <- get_estimates(fit3, setting3)
-D3 <- get_result_statistics_common(fit3, data3, setting3)
-write.csv(t(matrix(D3, nrow=2)), paste("output/realdata/MCMC_mayu/proposed/", model, "t_10.csv", sep=""))
-write.csv(t(matrix(est_param3$theta, nrow=34)), paste( "output/realdata/parameters/mayu/proposed/", model, "_theta_t_10.csv", sep=""))
-write.csv(t(matrix(est_param3$alpha_r, nrow=34)), paste( "output/realdata/parameters/mayu/proposed/", model, "_alpha_r_t_10.csv", sep=""))
-write.csv(t(matrix(est_param3$beta_rt, nrow=34)), paste( "output/realdata/parameters/mayu/proposed/", model, "_beta_rt_10.csv", sep=""))
-write.csv(t(matrix(est_param3$beta_rk, nrow=34)), paste( "output/realdata/parameters/mayu/proposed/", model, "_beta_rk_10.csv", sep=""))
-
-
-#est_param4 <- get_estimates(fit4, setting)
-#D4 <- get_result_statistics_common(fit4, data4, setting)
-#write.csv(t(matrix(D4, nrow=2)), paste(dir, "output/realdata/MCMC_statistics/", model, "4.csv", sep=""))
-
-wbic3 <- -mean(rowSums(extract(fit1)$log_lik))
-wbic5 <- -mean(rowSums(extract(fit2)$log_lik))
-wbic10 <- -mean(rowSums(extract(fit3)$log_lik))
-
-wbic3
-wbic5
-wbic10
-
-SD <- summary(fit1, par="theta")$summary[,c("sd", "se_mean")]
+SD <- summary(fit, par="theta")$summary[,c("sd", "se_mean")]
 apply(SD, 2, mean)
 write.csv(SD, paste(dir, "output/realdata/SE_mayu/", model, "1.csv", sep=""))
 
@@ -77,11 +44,11 @@ sink(paste(dir, "output/realdata/parameters/mayu/proposed/", model, "_param.txt"
 sink()
 
 # 結果描画用
-plot(est_param1$theta)
-plot(est_param1$alpha_r)
-est_param1$beta_rt
-for(r in 1:10){
-  plot(est_param1$beta_rt[r,],xlab="TimeID",ylab="beta_rt", type="l",xlim=c(1, 4), xaxp=c(1, 3, 2), ylim=c(-1,1), col=gray(0.8), lwd = 1)
+plot(est_param$theta)
+plot(est_param$alpha_r)
+est_param$beta_rt
+for(r in 1:setting$n_rater){
+  plot(est_param$beta_rt[r,],xlab="TimeID",ylab="beta_rt", type="l",xlim=c(1, 4), xaxp=c(1, 4, 3), ylim=c(-1.5,1), col=r, lwd = 1)
   par(new=T)
   }
 
